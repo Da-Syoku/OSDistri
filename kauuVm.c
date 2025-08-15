7a5a3b52673472c802808f4b081f5c24a237a7d3
Da-Syoku <snnnnsnn3777@gmail.com>
2025-08-15 19:31:07 +0900

    browser_winを消してみた

--- kauuVm.c
+++ kauuVm.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+Display dis;
+
 void open_bro() {
 
 	pid_t pid = fork();
@@ -11,7 +13,7 @@ void open_bro() {
 		perror("cant get proccese id!");
 		exit(1);
 	} else if (pid ==0) {
-		execlp("chromium", "chromium", NULL); //execlp is function to  replace prosses for new one
+		execlp("chromium", "chromium", "--no-sandbox", NULL); //execlp is function to  replace prosses for new one
 
 		perror("cant open browser!");
 		exit(1);
@@ -19,15 +21,48 @@ void open_bro() {
 }
 
 int main(void) {
-	Display *dis;
+	
 	XEvent eve;
 
 	dis = XOpenDisplay( NULL );
+	if (dis == NULL){
+		fprintf(stderr, "cannot open display\n");
+	}
+
+	XSelectInput(dis, SubstructureRedirectMask | SubstructureNotifyMask);
 
 	open_bro();
 
 	while (1) {
 		XNextEvent(dis, &eve);
+		if (eve.type == MapRequest) {
+            // 新しいウィンドウが表示されようとしている
+            XMapRequestEvent *map_req = &eve.xmaprequest;
+
+            // まだブラウザを管理していなければ、これを管理対象にする
+            if (dis == None) {
+                dis = map_req->window;
+
+                // 画面サイズを取得
+                int screen_width = DisplayWidth(dis, DefaultScreen(dis));
+                int screen_height = DisplayHeight(dis, DefaultScreen(dis));
+                
+                // ウィンドウを画面全体にリサイズして移動させる
+                XMoveResizeWindow(dis, 0, 0, screen_width, screen_height);
+                
+                // ウィンドウの表示を許可する
+                XMapWindow(dis);
+            }
+        } else if (eve.type == DestroyNotify) {
+            // ウィンドウが破棄された
+            XDestroyWindowEvent *destroy_ev = &eve.xdestroywindow;
+
+            // 破棄されたのが管理していたブラウザのウィンドウなら
+            if (destroy_ev->window == dis) {
+                dis = None; // 管理対象がいなくなったのでリセット
+                open_bro();         // 再度ブラウザを起動
+            }
+        }
 	}
 	XCloseDisplay(dis);
 	return 0;
