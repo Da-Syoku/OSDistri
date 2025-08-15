bf99502fcea45e034fd992a602dfa4ac24e4fa50
Da-Syoku <snnnnsnn3777@gmail.com>
2025-08-15 11:24:46 +0900

    fixxed error

--- kauuVm.c
+++ kauuVm.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+
+
+
 void open_bro() {
 
 	pid_t pid = fork();
@@ -11,7 +14,7 @@ void open_bro() {
 		perror("cant get proccese id!");
 		exit(1);
 	} else if (pid ==0) {
-		execlp("chromium", "chromium", "--no-sandbox" NULL); //execlp is function to  replace prosses for new one
+		execlp("chromium", "chromium", "--no-sandbox", NULL); //execlp is function to  replace prosses for new one
 
 		perror("cant open browser!");
 		exit(1);
@@ -20,27 +23,30 @@ void open_bro() {
 
 int main(void) {
 	Window root;
+	Window browser_win = None;
 	XEvent eve;
+	Display* dis;
 
 	dis = XOpenDisplay( NULL );
 	if (dis = NULL) {
 		fprintf(stderr, "Cannot open display\n");
 		exit(1);
 	}
-	root = defaultRootWindow(dis);
+	root = DefaultRootWindow(dis);
 	XSelectInput(dis, root, SubstructureRedirectMask | SubstructureNotifyMask);
 	open_bro();
 
 	while (1) {
 		XNextEvent(dis, &eve);//waiting while next event
 		if (eve.type == MapRequest){
+
 			XMapRequestEvent *map_req = &eve.xmaprequest;
 			if (browser_win == None){
 				browser_win = map_req->window;
 				int screenw = DisplayWidth(dis, DefaultScreen(dis));
 				int screenh = DisplayHeight(dis, DefaultScreen(dis));
 				XMoveResizeWindow(dis, browser_win, 0, 0, screenw, screenh);
-				XMapWindow(dis, Browser_win);
+				XMapWindow(dis, browser_win);
 			}
 		} else if (eve.type == DestroyNotify){
 			XDestroyWindowEvent *des_ev = &eve.xdestroywindow;
